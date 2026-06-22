674ce9021134c778960fc9890c146752f0c9fef1
habibullahsohu <m.habibullahsohu@gmail.com>
2026-06-22 14:52:58 +0500

    two_sum code

---
+++ two_sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+     int nums[5] = {0,2,4,6,8};
+        int a,b,sum;
+        cout<<"Enter 1st Number: ";
+        cin>>a;
+        cout<<"Enter 2nd Number: ";
+        cin>>b;
+        sum=a+b;
+        cout<<"Total Sum:"<<sum<<endl;
+        for(int i=0;i<=10;i++){
+            if(nums[i]==a){
+                cout<<"["<<i<<",";
+            }
+        }
+        for(int i=0;i<=10;i++){
+            if(nums[i]==b){
+                cout<<i<<"]";
+                break;
+            }
+        }
+    return 0;
+}
